d4cb46ad581e372ce8beeee80df8e4591e96b823
angeeeel <56625529+angeeeel@users.noreply.github.com>
2021-05-21 03:02:48 +0800

    Update ClassifierQuantizedMobileNet.java

--- app/src/main/java/org/tensorflow/lite/examples/classification/tflite/ClassifierQuantizedMobileNet.java
+++ app/src/main/java/org/tensorflow/lite/examples/classification/tflite/ClassifierQuantizedMobileNet.java
@@ -52,12 +52,12 @@ public class ClassifierQuantizedMobileNet extends Classifier {
     // you can download this file from
     // see build.gradle for where to obtain this file. It should be auto
     // downloaded into assets.
-    return "converted_tflite_quantized2/model_unquant.tflite";
+    return "converted_tflite_quantized/model_unquant.tflite";
   }
 
   @Override
   protected String getLabelPath() {
-    return "converted_tflite_quantized2/labels.txt";
+    return "converted_tflite_quantized/labels.txt";
   }
 
   @Override
